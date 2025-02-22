64be0e5d117b323e333cded9e9b073346d8506e2
Hudson <ardroneheart@gmail.com>
2025-02-22 15:07:20 -0500

    Bug fix: prevented backticks showing in code blocks

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,21 +1,5 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  theme: {
-    extend: {
-      typography: {
-        DEFAULT: {
-          css: {
-            'code::before': {
-              content: '""'
-            },
-            'code::after': {
-              content: '""'
-            }
-          }
-        }
-      },
-    },
-  },
   plugins: {
     '@tailwindcss/postcss': {},
   },
